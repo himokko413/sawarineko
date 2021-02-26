e937da93ddeb7e20387f88fb675c8611148b793d
Lucina Shirakawa Paige <lucina@nlpaige.me>
2021-02-26 22:30:12 +0900

    thanks for the restructure, aya!

---
+++ m/0984/dep.sh
@@ -0,0 +1,47 @@
+export SWR_IGNORE=true
+# Primary name to show on introductions, registered onto PK
+export SWR_NAME="Marle"
+# Full name, may be used in intro
+export SWR_FULL_NAME="Princess Nadia \'Marle\'"
+# Descriptive title on introductions, a la Fire Emblem Heroes
+export SWR_EPITHET="Free-Spirited Archer"
+# Pronouns (will also register to PK)
+export SWR_PRONOUNS="she/her"
+# Personal pronouns (i.e. watashi, watakushi, boku, ore)
+export SWR_PERSONAL="watashi"
+# Type of member (introject, walk-in, etc.)
+export SWR_TYPE="amalgamate"
+# Source (meant for introjects but can also describe reason for formation)
+export SWR_SOURCE="Lucy P, Hina, and Tsubasa O"
+# Source explanation (character page for fictives, Blossom definition etc.)
+export SWR_SURL=""
+# Color of member or member's cluster
+export SWR_COLOR="#333"
+# Role of member in system
+export SWR_ROLE="combined presence"
+# Age/birthday to display on intro. Appended to relative age if enabled
+export SWR_AGE="19 (2001.05.08)"
+# Birthday to add to PK
+export SWR_PK_BIRTHDAY="05-08"
+# Weapon color (old Grima protocol). 0 1 2 3 = red, blue, green, colorless
+export GRI_COL="3"
+# Weapon type (old Grima protocol). 0 1 2 3 4 5 6 7 8 = sword, lance, axe, staff, bow, dagger, tome, breath, beast
+export GRI_WEP="6"
+# Movement type (old Grima protocol). 0 1 2 3 = infantry, cavalry, armored, flier
+export GRI_MOV="2"
+# Name to show on bars (new intros) (JPN)
+export GRI_NAME=" "
+# Epithet to show on bars (new intros) (JPN)
+export GRI_EPITHET=" "
+# Description (preferably written by member)
+export SWR_DESC="what happens when Lucy, Hina, and Tsubasa make a combined member? cute happens! and i’m cute! i'm Akari, and i love to nerd out, sing, and stream!"
+# Proxy used on PK.
+export SWR_PROXY="as/text"
+# Source of avatar
+export SWR_ASOURCE="original avatar made in VRoid Studio"
+# URL of avatar source (if available)
+export SWR_AURL="https://vroid.com/studio"
+# Source of portrait
+export SWR_PSOURCE="original avatar made in VRoid Studio"
+# URL of portrait source (if available)
+export SWR_PURL="https://vroid.com/studio"
